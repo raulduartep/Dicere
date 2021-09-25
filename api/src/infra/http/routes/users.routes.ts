b33187b2db7d7314f4be8310ac161526fc2443e5
Raul Duarte Pereira <raulduartepe@gmail.com>
2021-09-25 16:31:01 -0300

    refactor: add controller to get user info feature

--- api/src/infra/http/routes/users.routes.ts
+++ api/src/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateUserRequestControllerHttp } from '@modules/users/useCases/createUserRequest/CreateUserRequestControllerHttp';
+import { GetMeControllerHttp } from '@modules/users/useCases/getMe/GetMeControllerHttp';
 import { ResetPasswordControllerHttp } from '@modules/users/useCases/resetPassword/ResetPasswordControllerHttp';
 import { SendForgotPasswordController } from '@modules/users/useCases/sendforgotPasswordEmail/SendForgotPasswordController';
 import { VerifyUserControllerHttp } from '@modules/users/useCases/verifyUser/VerifyUserControllerHttp';
@@ -13,6 +14,7 @@ const createUserRequestController = new CreateUserRequestControllerHttp();
 const sendForgotPasswordController = new SendForgotPasswordController();
 const resetPasswordController = new ResetPasswordControllerHttp();
 const verifyUserController = new VerifyUserControllerHttp();
+const getMeController = new GetMeControllerHttp();
 
 usersRouter.post('/', createUserRequestController.handle);
 
@@ -22,6 +24,6 @@ usersRouter.post('/reset_password', resetPasswordController.handle);
 
 usersRouter.post('/verify', verifyUserController.handle);
 
-usersRouter.get('/me', ensureAuthenticate);
+usersRouter.get('/me', ensureAuthenticate, getMeController.handle);
 
 export { usersRouter };
